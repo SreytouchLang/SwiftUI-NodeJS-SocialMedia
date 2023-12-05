d9e647bb0023275c892b5bac039ed994b1e892a7
SreytouchLang <atouch.it@gmail.com>
2023-12-05 13:29:58 -0800

    Initial Commit

---
+++ SwiftUI-NodeJS-SocialMedia/SwiftUI_NodeJS_SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NodeJS_SocialMediaApp.swift
+//  SwiftUI-NodeJS-SocialMedia
+//
+//  Created by Sreytouch(Jessica) on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NodeJS_SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
